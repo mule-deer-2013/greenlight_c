9ca2bde542a136565596923c124f63a5c47f5587
Dev Bootcamp <Apprentice@dbc34.local>
2013-09-30 15:33:22 -0700

    created user from client side form

--- js/greenlight.js
+++ js/greenlight.js
@@ -1,6 +1,6 @@
 var Greenlight = {
   init: function() {
-    $.ajax({
+      $.ajax({
       url: 'http://localhost:3000/',
       success: function(data) {
         var source   = $("#entry-template").html();
@@ -10,6 +10,43 @@ var Greenlight = {
     })
   }
 }
+
+
+
+
+
+
 $(document).ready(function(){
-  Greenlight.init();
-})
+  $('#signup').on("click", signup);
+  $("#profileform").on("submit", function(){
+    console.log("clicked submit");
+  });
+});
+
+
+var signup = function() {
+  $('#signup').toggle();
+  var source   = $("#signup-template").html();
+  var template = Handlebars.compile(source);
+  $('body').append(template);
+  $("#profileform").on("submit", createUser);
+  };
+
+
+var createUser = function(e) {
+
+    e.preventDefault();
+    console.log("I'm in the submit function");
+    var postData = $(this).serialize();
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      type: "POST",
+      data: postData,
+      success: function(data){
+        console.log(data)
+      }
+    })
+
+};
+
+
