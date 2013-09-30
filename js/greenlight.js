498caa96ed9ee209e9e00db09b71b946dbef77a8
Apprentice <apprentice@dbc23.local>
2013-09-30 15:44:41 -0700

    added basic profile view

--- js/greenlight.js
+++ js/greenlight.js
@@ -5,11 +5,34 @@ var Greenlight = {
       success: function(data) {
         var source   = $("#entry-template").html();
         var template = Handlebars.compile(source);
-        $('body').append(template({body: data.hey}));
+        $('body').append(template({body: data}));
       }
     })
   }
 }
+
+var Showprofile = {
+  init: function() {
+    $.ajax({
+      url: 'http://localhost:3000/users/1',
+      type: "GET",
+      success: function(data) {
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        $('body').append(template({
+          name: data.user.name, 
+          age: data.user.age,
+          sex: data.user.sex, 
+          sex_preference: data.user.sex_preference 
+          })
+        );
+      }
+    })
+  }
+}
+
+
 $(document).ready(function(){
-  Greenlight.init();
+  //Greenlight.init();
+  Showprofile.init();
 })
