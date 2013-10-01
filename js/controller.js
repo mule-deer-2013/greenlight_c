4db9da2b170ca98555fa87d91d50eafad269bcc3
Apprentice <apprentice@dbc23.local>
2013-10-01 12:08:25 -0700

    add oojs

---
+++ js/controller.js
@@ -0,0 +1,63 @@
+var Controller = function(baseUrl) {
+  this.baseUrl = baseUrl;
+
+  this.initialize();
+};
+
+Controller.prototype = {
+  initialize: function() {
+    var self = this;
+    $(document).on('click', '#green-button', function() { self.vote("yes") });
+    $(document).on('click', '#red-button', function() { self.vote("no") });
+    $(document).on('submit', "#profileform", function() { self.signup() });
+  },
+
+  vote: function(opinion) {
+    var self = this;
+    var vote = new Object();
+    vote['voted_on_id'] = $('.user').data('id');
+    vote['voter_id'] = localStorage['currentUser'];
+    vote['opinion'] = opinion;
+    $.post(this.baseUrl + '/votes', vote)
+    .done(function(response) {
+      // ignore the response, show the next user
+      self.getRandomUser();
+    });
+  },
+
+  getRandomUser: function() {
+    var self = this;
+    var templateSelector = "#profile-template";
+    $.ajax({url: self.baseUrl + '/users/random'})
+    .done(function(data) {
+      var user = new User(data);
+      self.render(templateSelector, user);
+      // $('#greenbutton').on('click', voteOnProfile); 
+      //getLocation()
+    });
+  },
+
+  signup: function() {
+    e.preventDefault();
+    var postData = new FormData($('form')[0]);
+    $.ajax({
+      url: this.baseUrl + '/users',
+      type: "POST",
+      data: postData,
+      cache: false,
+      contentType: false,
+      processData: false
+    })
+    .done(function(data) {
+      localStorage['currentUser']= data.id 
+      $('.signupform').toggle();
+      self.getRandomUser();
+    });
+  },
+
+  render: function(templateSelector, data) {
+    var source   = $(templateSelector).html();
+    var template = Handlebars.compile(source);
+    $('body').html(template(data));
+  }
+}
--- js/greenlight.js
+++ js/greenlight.js
@@ -1,97 +1,127 @@
 // we're not doing anything with this ajax call right now
 
-var Showprofile = {
-  init: function() {
-    console.log("im in the showprofile function");
-    $.ajax({
-      url: 'http://localhost:3000/users/1', //hard coded; need to fix
-      type: "GET",
-      success: function(data) {
-        console.log(data);
-        var source = $("#entry-template").html();
-        var template = Handlebars.compile(source);
-        $('body').append(template({
-          name: data.user.name, 
-          age: data.user.age,
-          sex: data.user.sex, 
-          sex_preference: data.user.sex_preference,
-          photo: data.user.photo 
-          })
-        );
-      }
-    })
-  }
-}
-//
+// var Showprofile = {
+//   init: function() {
+//     console.log("im in the showprofile function");
+//     $.ajax({
+//       url: 'http://localhost:3000/users/1', //hard coded; need to fix
+//       type: "GET",
+//       success: function(data) {
+//         console.log(data);
+//         var source = $("#entry-template").html();
+//         var template = Handlebars.compile(source);
+//         $('body').append(template({
+//           name: data.user.name, 
+//           age: data.user.age,
+//           sex: data.user.sex, 
+//           sex_preference: data.user.sex_preference,
+//           photo: data.user.photo 
+//         })
+//         );
+//       }
+//     })
+//   }
+// }
+// //
 
-$(document).ready(function(){
-  var userId = localStorage['currentUser']
-  $('#signup').on("click", signup);
+
+$(function() {
+  var controller = new Controller('http://localhost:3000');
+  if (localStorage['currentUser']) {
+    controller.getRandomUser();
+  } else {
+    var source   = $("#signup-template").html();
+    var template = Handlebars.compile(source);
+    $('body').append(template);
+  }
 });
 
 
-var signup = function() {
-  $('#signup').toggle();
-  var source   = $("#signup-template").html();
-  var template = Handlebars.compile(source);
-  $('body').append(template);
-  $("#profileform").on("submit", createUser);
+// var signup = function() {
+//   $('#signup').toggle();
+//   var source   = $("#signup-template").html();
+//   var template = Handlebars.compile(source);
+//   $('body').append(template);
+//   $("#profileform").on("submit", createUser);
 
-  };
+// };
 
 
-var createUser = function(e) {
+// var createUser = function(e) {
 
-    e.preventDefault();
-    console.log("I'm in the submit function");
-    var postData = new FormData($('form')[0]);
-    $.ajax({
-      url: 'http://localhost:3000/users',
-      type: "POST",
-      data: postData,
-      //Options to tell JQuery not to process data or worry about content-type
-      cache: false,
-      contentType: false,
-      processData: false,
-      success: function(data){
-        console.log(data);
-        $('.signupform').toggle();
-         $('#start_looking').on('click',showUsers());
-         localStorage['currentUser']= data.id 
-      }
-    })
+//   e.preventDefault();
+//   console.log("I'm in the submit function");
+//   var postData = new FormData($('form')[0]);
+//   $.ajax({
+//     url: 'http://localhost:3000/users',
+//     type: "POST",
+//     data: postData,
+//       //Options to tell JQuery not to process data or worry about content-type
+//       cache: false,
+//       contentType: false,
+//       processData: false,
+//       success: function(data){
+//         console.log(data);
+//         $('.signupform').toggle();
+//         $('#start_looking').on('click',showUsers());
+//         localStorage['currentUser']= data.id 
+//       }
+//     })
 
-};
+// };
 
- var showUsers = function() {
+// var showUsers = function() {
+//   $.ajax({url: 'http://localhost:3000/users'})
+//   .done(function(data) {
+//     var source   = $("#profile-template").html();
+//     var template = Handlebars.compile(source);
+//     //photo is coming back undefined here
+//     console.log(data);
+//     $('body').html(template({name: data.user.name, age: data.user.age, tagline: data.user.tagline, photo: data.photo, id: data.user.id}));
+//     $('#greenbutton').on('click', voteOnProfile); 
+//     //getLocation()
+//   })
+//   .fail(function() {
+//     console.log("FUck you Jeffrey");
+//   });
+// };
 
-  $.ajax({
-    url: 'http://localhost:3000/users',
-    success: function(data) {
-      var source   = $("#profile-template").html();
-      var template = Handlebars.compile(source);
-    //photo is coming back undefined here
-      console.log(data);
-      $('body').html(template({name: data.user.name, age: data.user.age, tagline: data.user.tagline, photo: data.photo, id: data.user.id}));
-      $('#greenbutton').on('click', voteOnProfile); 
-    }
-  });
-};
+// var voteOnProfile = function() {
+//   var idsHash = new Object();
+//   idsHash['voted_on_id'] = $('.body').data('id');
+//   idsHash['voter_id'] = localStorage['currentUser'];
+//   idsHash['opinion'] = "yes";
+//   $.ajax({
+//     url: 'http://localhost:3000/votes',
+//     type: "POST",
+//     dataType: "JSON",
+//     data: idsHash,
+//     success: function(data) {
+//       console.log(data);
+//       showUsers();
+//     }
+//   })
+// }
 
-var voteOnProfile = function() {
-  var idsHash = new Object();
-  idsHash['voted_on_id'] = $('.body').data('id');
-  idsHash['voter_id'] = localStorage['currentUser'];
-  idsHash['opinion'] = "yes";
-  $.ajax({
-    url: 'http://localhost:3000/votes',
-    type: "POST",
-    dataType: "JSON",
-    data: idsHash,
-    success: function(data) {
-      console.log(data);
-      showUsers();
-    }
-  })
-}
+// // var getLocation = function() {
+// //   if (geo_position_js.init()) {
+// //     geo_Position_js.getCurrentPosition(geoSuccess, geoError);
+// //   }
+// //   else{
+// //     alert("Functionality not available")
+// //   }
+
+// //   function geoSuccess(p) {
+// //     var latitude = p.coords.latitude
+// //     var longtitude = p.coords.longtitude
+// //     alert(latitude)
+// //   }
+  
+// //   function geoError(){
+// //     alert('please allow access to your location')
+// //   }
+// // }
+
+
+
 
