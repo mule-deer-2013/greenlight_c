06697017120aba44c7c293ca936579cac144ed30
Apprentice <apprentice@dbc23.local>
2013-09-30 21:04:51 -0700

    fixed merge conflicts and redirect to random user after signup

--- js/greenlight.js
+++ js/greenlight.js
@@ -1,8 +1,31 @@
+// we're not doing anything with this ajax call right now
 
+var Showprofile = {
+  init: function() {
+    console.log("im in the showprofile function");
+    $.ajax({
+      url: 'http://localhost:3000/users/1', //hard coded; need to fix
+      type: "GET",
+      success: function(data) {
+        console.log(data);
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        $('body').append(template({
+          name: data.user.name, 
+          age: data.user.age,
+          sex: data.user.sex, 
+          sex_preference: data.user.sex_preference,
+          photo: data.user.photo 
+          })
+        );
+      }
+    })
+  }
+}
+//
 
 $(document).ready(function(){
   $('#signup').on("click", signup);
-
 });
 
 
@@ -12,23 +35,29 @@ var signup = function() {
   var template = Handlebars.compile(source);
   $('body').append(template);
   $("#profileform").on("submit", createUser);
-};
+  };
 
 
 var createUser = function(e) {
 
-  e.preventDefault();
-  console.log("I'm in the submit function");
-  var postData = $(this).serialize();
-  $.ajax({
-    url: 'http://localhost:3000/users',
-    type: "POST",
-    data: postData,
-    success: function(data){
+    e.preventDefault();
+    console.log("I'm in the submit function");
+    var postData = new FormData($('form')[0]);
+    $.ajax({
+      url: 'http://localhost:3000/users',
+      type: "POST",
+      data: postData,
+      //Options to tell JQuery not to process data or worry about content-type
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(data){
+        $('.signupform').toggle();
+        Showprofile.init();
+         $('#start_looking').on('click',showUsers());
+      }
+    })
 
-     showUsers()
-    }
-  });
 };
 
  var showUsers = function() {
@@ -56,8 +85,4 @@ var voteOnProfile = function() {
     }
   })
 }
-
-
-
-
 
