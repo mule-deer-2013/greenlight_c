0b72a25063dca8306f35bfe8fce92b048db46243
Dev Bootcamp <Apprentice@dbc34.local>
2013-09-30 19:53:34 -0700

    add voting

--- js/greenlight.js
+++ js/greenlight.js
@@ -11,16 +11,12 @@ var Greenlight = {
   }
 }
 
-
-
-
-
-
 $(document).ready(function(){
   $('#signup').on("click", signup);
   $("#profileform").on("submit", function(){
-    console.log("clicked submit");
+
   });
+
 });
 
 
@@ -30,23 +26,52 @@ var signup = function() {
   var template = Handlebars.compile(source);
   $('body').append(template);
   $("#profileform").on("submit", createUser);
-  };
+};
 
 
 var createUser = function(e) {
 
-    e.preventDefault();
-    console.log("I'm in the submit function");
-    var postData = $(this).serialize();
-    $.ajax({
-      url: 'http://localhost:3000/users',
-      type: "POST",
-      data: postData,
-      success: function(data){
-        console.log(data)
-      }
-    })
-
+  e.preventDefault();
+  console.log("I'm in the submit function");
+  var postData = $(this).serialize();
+  $.ajax({
+    url: 'http://localhost:3000/users',
+    type: "POST",
+    data: postData,
+    success: function(data){
+    localStorage.setItem('flo')
+    showUsers()
+    }
+  });
 };
+
+ var showUsers = function() {
+
+  $.ajax({
+    url: 'http://localhost:3000/users',
+    success: function(data) {
+      var source   = $("#profile-template").html();
+      var template = Handlebars.compile(source);
+      $('body').html(template({name: data.name, id: data.id}));
+      $('#greenbutton').on('click', voteOnProfile);
+    }
+  });
+};
+
+var voteOnProfile = function() {
+  var profileID = $('.body').data('id');
+  console.log(profileID);
+  $.ajax({
+    url: 'http://localhost:3000/votes',
+    type: "POST",
+    data: profileID,
+    success: function() {
+      showUser()
+    }
+  })
+}
+
+
+
 
 
