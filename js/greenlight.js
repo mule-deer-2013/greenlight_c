8da23bdec55ababdf739930a2dbf3880b7631771
Apprentice <apprentice@dbc23.local>
2013-09-30 19:51:47 -0700

    photos showing with s3 and paperclip

--- js/greenlight.js
+++ js/greenlight.js
@@ -11,37 +11,28 @@ var Greenlight = {
   }
 }
 
-<<<<<<< HEAD
 var Showprofile = {
   init: function() {
+    console.log("im in the showprofile function");
     $.ajax({
-      url: 'http://localhost:3000/users/1',
+      url: 'http://localhost:3000/users/1', //hard coded; need to fix
       type: "GET",
       success: function(data) {
+        console.log(data);
         var source = $("#entry-template").html();
         var template = Handlebars.compile(source);
         $('body').append(template({
           name: data.user.name, 
           age: data.user.age,
           sex: data.user.sex, 
-          sex_preference: data.user.sex_preference 
+          sex_preference: data.user.sex_preference,
+          photo: data.user.photo 
           })
         );
       }
     })
   }
 }
-
-
-$(document).ready(function(){
-  //Greenlight.init();
-  Showprofile.init();
-})
-=======
-
-
-
-
 
 $(document).ready(function(){
   $('#signup').on("click", signup);
@@ -64,17 +55,20 @@ var createUser = function(e) {
 
     e.preventDefault();
     console.log("I'm in the submit function");
-    var postData = $(this).serialize();
+    var postData = new FormData($('form')[0]);
     $.ajax({
       url: 'http://localhost:3000/users',
       type: "POST",
       data: postData,
+      //Options to tell JQuery not to process data or worry about content-type
+      cache: false,
+      contentType: false,
+      processData: false,
       success: function(data){
-        console.log(data)
+        $('.signupform').toggle();
+        Showprofile.init();
       }
     })
 
 };
 
-
->>>>>>> 9ca2bde542a136565596923c124f63a5c47f5587
